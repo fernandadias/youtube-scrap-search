fa3e4dc9a72a063d328fde67e359f4c2987c356c
Fernanda Dias <diasfeh@gmail.com>
2021-07-19 22:44:47 -0300

    Versão com o baseUrl

--- get-lives.js
+++ get-lives.js
@@ -1,33 +1,79 @@
+const fs = require('fs');
+const stringify = require('csv-stringify');
 const puppeteer = require('puppeteer');
-const url = 'https://www.youtube.com/results?search_query=live&sp=CAMSBggFEAEYAg%253D%253D';
+const baseEmpty = 'not-found'
+const baseUrl = 'https://www.youtube.com/'
+const search = 'results?search_query=live&sp=CAMSBggFEAEYAg%253D%253D';
 
 (async () => {
     // Start puppeteer
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+    page.setViewport({ width: 1280, height: 926 });
+
+    // Navigate to the demo page.
+    await page.goto(baseUrl+search);
+
+    await page.waitForSelector('.ytd-item-section-renderer', { timeout: 10000 });
     
     // get related infos
     const lives = await page.evaluate(() => 
         Array.from(document.querySelectorAll('#contents #contents .ytd-item-section-renderer'))
             .map(live => ({
-                live_link:          live.querySelector(".ytd-video-renderer #thumbnail").href || 'not-found',
-                live_thumbnail:     live.querySelector(".ytd-video-renderer #thumbnail.img.style-scope").href || 'not-found',
-                live_views:         live.querySelector("#metadata-line span:nth-child(1)").innerText.trim() || 'not-found',
-                live_date:          live.querySelector("#metadata-line span:nth-child(2)").innerText.trim() || 'not-found',
+                live_name:          live.querySelector("#video-title").getAttribute("href") || baseEmpty,
+                live_link:          live.querySelector("#video-title").getAttribute("href") || baseEmpty,
+                live_thumbnail:     live.querySelector(".ytd-thumbnail .yt-img-shadow").getAttribute("href") || baseEmpty,
+                live_views:         live.querySelector("#metadata-line span:nth-child(1)").innerText.trim() || baseEmpty,
+                live_date:          live.querySelector("#metadata-line span:nth-child(2)").innerText.trim() || baseEmpty,
                 live_type:          '',
-                channel_thumbnail:  live.querySelector("#channel-info .ytd-video-renderer.img.yt-img-shadow").src || 'not-found',
-                channel_name:       live.querySelector("#channel-name .yt-simple-endpoint").innerText.trim() || 'not-found',
-                channel_link:       live.querySelector("#channel-name .yt-simple-endpoint").href || 'not-found',
+                channel_thumbnail:  live.querySelector("img.yt-img-shadow").getAttribute("src") || baseEmpty,
+                channel_name:       live.querySelector("#channel-name .yt-simple-endpoint").innerText.trim() || baseEmpty,
+                channel_link:       live.querySelector("#channel-name .yt-simple-endpoint").getAttribute("href") || baseEmpty,
             }))
         )
 
-    // check if video are from a streaming or not
     lives.map(live => (
-        live.live_date.indexOf('Streamed') == 0 ? live.live_type = 'Stream' : live.live_type = 'Other',
-        live.live_date = live.live_date.replace('Streamed ','')
+      // check if video are from a streaming or not
+      live.live_date.indexOf('Streamed') == 0 ? live.live_type = 'Stream' : live.live_type = 'Other',
+      live.live_date = live.live_date.replace('Streamed ',''),
+
+      //put baseUrl
+      live.live_link !== baseEmpty && baseUrl+live.live_link,
+      live.live_thumbnail !== baseEmpty && baseUrl+live.live_thumbnail,
+      live.channel_thumbnail !== baseEmpty && baseUrl+live.channel_thumbnail,
+      live.channel_link !== baseEmpty && baseUrl+live.channel_link
     ))
+
+    const columns = {
+      live_name: 'live_name', 
+      live_link: 'live_link', 
+      live_thumbnail: 'live_thumbnail', 
+      live_views: 'live_views', 
+      live_date: 'live_date', 
+      live_type: 'live_type', 
+      channel_thumbnail: 'channel_thumbnail', 
+      channel_name: 'channel_name', 
+      channel_link: 'channel_link'};
     
     console.log(lives),
+
+    // stringify(lives, { header: true, columns: columns }, function(err, output) {
+    //   fs.writeFile('lives.csv', output, 'utf8', function(err) {
+    //     if (err) {
+    //       console.log('Some error occured - file either not saved or corrupted file saved.');
+    //     } else {
+    //       console.log('It\'s saved!');
+    //     }
+    //   });
+    // });
+
+    stringify(lives, { header: true, columns: columns }, (err, output) => {
+      if (err) throw err;
+      fs.writeFile('lives.csv', output, (err) => {
+        if (err) throw err;
+        console.log('my.csv saved.');
+      });
+    });
 
     await browser.close();
 })();
